5954e4c74ee57d19f1acefca6ca24423901367d0
Osama M.Younus <osamayounus541@gmail.com>
2022-08-26 13:19:02 +0500

    project to do complete Desgin is rem

--- app.js
+++ app.js
@@ -4,9 +4,41 @@ var toDoListItem = document.getElementById("toDoList");
 function addtodo(){
 var toDoitem = document.getElementById("todo");
 
+// create a list
 var li = document.createElement('li');
 var liText = document.createTextNode(toDoitem.value);
 li.appendChild(liText);
 toDoListItem.appendChild(li);
 toDoitem.value = "";
+
+
+// add a delete button
+var del = document.createElement("BUTTON");
+var delTxt = document.createTextNode('Delete');
+del.setAttribute("onclick","deletToDo(this)")
+del.appendChild(delTxt);
+li.appendChild(del);
+
+// add an edit button
+var editbtn = document.createElement("BUTTON");
+var editTxt = document.createTextNode("Edit");
+editbtn.setAttribute("onclick","editToDo(this)");
+editbtn.appendChild(editTxt);
+li.appendChild(editbtn);
+
 }
+
+// delet button 
+function deletToDo(d){
+    d.parentNode.remove();
+    
+}
+
+// edit button
+
+function editToDo(e){
+     var userInput = prompt("Enter New To Do",e.parentNode.firstChild.nodeValue);
+     e.parentNode.firstChild.nodeValue = userInput;
+    
+}
+
